04f53776d34bb30c49c80dd674efbed4c50d7917
YegorCreative <aleqsandregor@gmail.com>
2026-06-05 22:28:47 -0500

    Initial SwiftUI app setup

---
+++ LetUsPray/LetUsPray/LetUsPrayApp.swift
@@ -0,0 +1,17 @@
+//
+//  LetUsPrayApp.swift
+//  LetUsPray
+//
+//  Created by Yegor Hambaryan on 6/5/26.
+//
+
+import SwiftUI
+
+@main
+struct LetUsPrayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
